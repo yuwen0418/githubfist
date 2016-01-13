0e9cb6feaea4eaab73647a020e29f7d5412ba60a
yuwen0418 <mac@G-D.local>
2016-01-13 14:13:59 +0800

    第一次更新

---
+++ cocoapods/cocoapods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cocoapods
+//
+//  Created by ma c on 16/1/12.
+//  Copyright © 2016年 sxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cocoapods/cocoapods/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cocoapods
+//
+//  Created by ma c on 16/1/12.
+//  Copyright © 2016年 sxt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
